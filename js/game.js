cb886587ad632897cc28b4e03d34e947b02c4fb1
Guilhem SUSA <guilhem31.susa@hotmail.com>
2018-12-07 00:46:35 +0100

    Pinata move all around body

--- js/game.js
+++ js/game.js
@@ -1,25 +1,22 @@
-var isClicked = false;
+
 var pinatas = [];
 
 function launchGame(){
-  pinatas.push(pinata);
-  var pinata = document.querySelector(".imgSvg");
-  if(!isClicked){
-    agrandirImage(pinata);
-  }
-  while(pinatas.length!=0){
-    move(pinata)
-  }
+    var imgPinata = document.getElementById("imgSvg");
+    var divPinata = document.getElementById("divSvg");
+    pinatas.push(imgPinata);
+    while(pinatas.length!=0){
+        move(imgPinata,divPinata)
+        pinatas.splice(0, 1)
+    }
 }
 
-function agrandirImage(pinata){
-    isClicked = true
-  	pinata.setAttribute('width', 231);
-  	pinata.setAttribute('height',274);
-}
-
-function agrandirImage(pinata){
-  pinata.style.top = Math.random() * document.body.clientWidth + 'px';
-  pinata.style.left = Math.random() * document.body.clientHeight + 'px';
-  pinata.rotate(Math.random() * 360);
+function move(imgPinata,divPinata){
+    //divPinata.style.top = Math.random() * 100+'%';
+    //divPinata.style.left = Math.random() * 100+'%';
+    var newStyle = 'transform: translate3d('+Math.random() *  document.body.clientWidth+'px,'
+      + Math.random() *  document.body.clientHeight +'px,0px);';
+      + 'transform:rotate('+Math.random() * 360+'deg);'
+      + 'display: inline-block;'
+    divPinata.setAttribute('style',newStyle);
 }
